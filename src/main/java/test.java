5ad8788392cae3963b5f1022d1fab3c0fecdfc99
Dmitriy Kulinich <dmitriy.kulinich@together.com>
2019-09-04 16:19:59 +0300

    add log4j2

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,5 +1,37 @@
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class test {
+
+    static final Logger rootLogger = LogManager.getRootLogger();
+    static final Logger userLogger = LogManager.getLogger(User.class);
+
     public static void main(String[] args) {
-	System.out.println("Hi");
+	User user = new User();
+	user.setName("Anakin");
+	user.setLastName("Skywalker");
+
+	userLogger.info(user.showMeMessage());
+	userLogger.info(user.giveMeASign());
+
+	rootLogger.info("Root Logger: "  + user.showMeMessage());
+
+	//debug
+	if (rootLogger.isDebugEnabled()) {
+	    rootLogger.debug("RootLogger: In debug message");
+	    userLogger.debug("UserLogger in debug");
+	}
+
+	try {
+	    User userNull = new User();
+	    userNull.getName().toString();
+	} catch (NullPointerException ex) {
+	    userLogger.error("error message: " + ex.getMessage());
+	    userLogger.fatal("fatal error message: " + ex.getMessage());
+	}
+
+
     }
 }
