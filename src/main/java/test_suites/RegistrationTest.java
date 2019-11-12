af39fedae6a0b52e68114d98a2a142b876b4f868
Dmitriy Kulinich <dmitriy.kulinich@together.com>
2019-11-12 14:49:25 +0200

    up

--- src/main/java/test_suites/RegistrationTest.java
+++ src/main/java/test_suites/RegistrationTest.java
@@ -1,5 +1,7 @@
 package test_suites;
 
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
@@ -14,14 +16,22 @@ public class RegistrationTest extends TestSuiteBase {
 	String email = "dmitriykulinich" + (new Date()).getTime() + "@maildrop.ropot.net";
 	app().log().info(email);
 	String password = "asdasd123";
-	Object response = app().rest().request().body("UserForm[gender]=male&UserForm[sexual_orientation]=hetero&UserForm[age]=21&UserForm[location]=Dnipropetrovsk,"
+	String gender = "";
+	String sexual_orientation = "";
+	String age = "";
+	String location = "";
+	String termsConsent = "";
+	String policyConsent = "";
+
+	ValidatableResponse response = app().rest().request().body("UserForm[gender]=male&UserForm[sexual_orientation]=hetero&UserForm[age]=21&UserForm[location]=Dnipropetrovsk,"
 			+ "+49000&UserForm[email]=" + email + "&UserForm[password]=" + password + "&UserForm[termsConsent]=true&UserForm[policyConsent]=true&UserForm[lid"
 			+ "]=3830403ea31a11e9a8911402ec33333c&UserForm[landingVisitId]=4361e4417c576200f02c81c7ecc54eab&UserForm[splitId]=&UserForm[transferId]=b106b41c55f449ae84e2d050b981bed9")
 			.when()
 			.post("https://www.flirt.com/user/register")
 			.then()
 			.statusCode(302)
-			.header("location", Matchers.equalTo("https://www.flirt.com/confirmation"));
+			.header("location", Matchers
+			.equalTo("https://www.flirt.com/confirmation"));
 
     }
 }
