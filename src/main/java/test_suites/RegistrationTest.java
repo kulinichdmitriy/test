fe7ae49aa3d7d29218c6d6c5078f429389e01f01
Dmitriy Kulinich <dmitriy.kulinich@together.com>
2019-11-13 14:45:53 +0200

    up

--- src/main/java/test_suites/RegistrationTest.java
+++ src/main/java/test_suites/RegistrationTest.java
@@ -1,6 +1,5 @@
 package test_suites;
 
-import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import java.util.Date;
@@ -10,41 +9,7 @@ import static core.ApplicationManager.app;
 public class RegistrationTest extends TestSuiteBase {
 
     @Test
-   /* public void registration() {
-
-	app().userModel().setAge(21);
-	app().userModel().setEmail("dmitriykulinich" + (new Date()).getTime() + "@maildrop.ropot.net");
-	app().userModel().setGenger("male");
-	app().userModel().setPassword("asdasd123");
-	app().userModel().setLocation("Dnipropetrovsk,+49000");
-	app().userModel().setSexualOrientation("hetero");
-	Boolean termsConsent = true;
-	Boolean policyConsent = true;
-	String lid = "3830403ea31a11e9a8911402ec33333c";
-	String landingVisitId = "4361e4417c576200f02c81c7ecc54eab";
-	String transferId = "b106b41c55f449ae84e2d050b981bed9";
-	System.out.println("email - "+app().userModel().getEmail());
-
-	ValidatableResponse response = app().rest()
-			.request()
-			.body("UserForm[gender]=" + app().userModel().getGender()
-					+ "&UserForm[sexual_orientation]=" + app().userModel().getSexualOrientation()
-					+ "&UserForm[age]=" + app().userModel().getAge()
-					+ "&UserForm[location]=" + app().userModel().getLocation()
-					+ "&UserForm[email]=" + app().userModel().getEmail()
-					+ "&UserForm[password]=" + app().userModel().getPassword()
-					+ "&UserForm[termsConsent]=" + termsConsent
-					+ "&UserForm[policyConsent]=" + policyConsent
-					+ "&UserForm[lid]=" + lid
-					+ "&UserForm[landingVisitId]=" + landingVisitId
-					+ "&UserForm[transferId]=" + transferId)
-			.when()
-			.post("https://www.flirt.com/user/register")
-			.then()
-			.statusCode(302)
-			.header("location", Matchers.equalTo("https://www.flirt.com/confirmation"));
-    }*/
-    public void regAndGetToken() {
+    public void getUserAutologin() {
 	app().userModel().setAge(21);
 	app().userModel().setEmail("dmitriykulinich" + (new Date()).getTime() + "@maildrop.ropot.net");
 	app().userModel().setGenger("male");
@@ -58,7 +23,7 @@ public class RegistrationTest extends TestSuiteBase {
 	String transferId = "b106b41c55f449ae84e2d050b981bed9";
 	System.out.println("email - " + app().userModel().getEmail());
 
-	Response response = app().rest()
+	String getRefreshToken = app().rest()
 			.request()
 			.header("X-Requested-With", "XMLHttpRequest")
 			.body("UserForm[gender]=" + app().userModel().getGender()
@@ -80,5 +45,6 @@ public class RegistrationTest extends TestSuiteBase {
 			.response()
 			.jsonPath()
 			.get("data.refresh_token");
+
     }
 }
